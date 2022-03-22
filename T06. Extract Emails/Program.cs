50b4fc9dfed215c0f35ff610b15ba5caf186a9b2
Spasko Katsarski <patsokatsarski1@gmail.com>
2022-03-22 17:30:42 +0200

    Minor Change

--- T06. Extract Emails/Program.cs
+++ T06. Extract Emails/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace T06._Extract_Emails
 {
@@ -6,9 +7,19 @@ namespace T06._Extract_Emails
     {
         static void Main(string[] args)
         {
-            // Look and understand the following patter:
+            // Look and understand the following pattern:
             string pattern =
                 @"(^|(?<=\s))(([a-zA-Z0-9]+)([\.\-\_]?)([A-Za-z0-9]+)(@)([a-zA-Z]+([\.\-][A-Za-z]+)+))(\b|(?=\s))";
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                MatchCollection matches = Regex.Matches(command, pattern);
+                foreach (Match emailMatch in matches)
+                {
+                    Console.WriteLine(emailMatch.Value);
+                }
+            }
         }
     }
 }
